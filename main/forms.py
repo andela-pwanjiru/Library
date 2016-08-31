e59fbfcf3b6bd94320cd2f7d922ea3f8a13c2b46
Penina Wanjiru <penina.wanjiru@andela.com>
2016-08-31 17:14:42 +0300

    [Feature] Integrate search view with templates

--- main/forms.py
+++ main/forms.py
@@ -3,6 +3,11 @@ from main.models import Book, Category
 
 
 class BookForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields['category'].required = False
+        self.fields['title'].required = False
+
     class Meta:
         model = Book
         fields = ['title', 'category']
--- main/views.py
+++ main/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import View
 from main.forms import BookForm
+from main.models import Book, Category
 
 # Create your views here.
 class LibraryView(View):
     def get(self, request):
-        form = BookForm(request.GET)
-        if form.is_valid():
-            title = request.GET('title')
-            category = request.GET('category')
-            if title:
-                object_list = Book.objects.filter(title_icontains=title)
-            if category:
-                object_list = Book.objects.filter(category=category)
-            form = {'object_list': object_list}
-            return render(request, 'results.html', form)
-
-        context = {'form': form}
-        return render(request, 'search.html', context)
+        if request.GET:
+            form = BookForm(request.GET)
+            if form.is_valid():
+                # import ipdb; ipdb.set_trace()
+                book_name = form.cleaned_data.get('title')
+                category_name = form.cleaned_data.get('category')
+                if book_name:
+                    object_list = Book.objects.filter(title__icontains=book_name)
+                if category_name:
+                    cat_obj = Category.objects.filter(name=category_name)
+                    object_list = Book.objects.filter(category=cat_obj)
+                form = {'object_list': object_list}
+                return render(request, 'results.html', form)
+        form = {'form': BookForm}
+        return render(request, 'search.html', form)
